17a068c5852014c362abd6e6ea9ccc172c88a4ea
Eenvincible <elisha.java@gmail.com>
2013-03-06 17:17:31 -0800

    Add SSL in production

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,6 +21,6 @@ class User < ActiveRecord::Base
   validates :email, presence:   true,
                     format:     { with: VALID_EMAIL_REGEX },
                     uniqueness: { case_sensitive: false }
-  validates :password, presence: true, length: { minimum: 6 }
+  validates :password, length: { minimum: 6 } #changed presence: true - for password
   validates :password_confirmation, presence: true
 end
