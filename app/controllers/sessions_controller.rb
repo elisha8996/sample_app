0571acc63633bd5fb2f23a76eccc0b15ec1aafa1
Eenvincible <elisha.java@gmail.com>
2013-03-08 16:59:39 -0800

    Finish user edit, update, index and destroy actions

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -6,12 +6,12 @@ class SessionsController < ApplicationController
 		user = User.find_by_email(params[:session][:email].downcase)
         if user && user.authenticate(params[:session][:password])
            sign_in user
-           redirect_to user
+           redirect_back_or user
         else
-           flash.now[:error] = 'Invalid email/password combination' # Not quite right!
+           flash.now[:error] = 'Invalid email/password combination'
            render 'new'
         end
-	end
+    end
 	def destroy
 	   sign_out
        redirect_to root_url
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,21 @@ describe User do
 
   subject { @user }
 
+  it { should respond_to(:admin) }
+  it { should respond_to(:authenticate) }
+
+  it { should be_valid }
+  it { should_not be_admin }
+
+  describe "with admin attribute set to 'true'" do
+    before do
+      @user.save!
+      @user.toggle!(:admin)
+    end
+
+    it { should be_admin }
+  end
+
   it { should respond_to(:name) }
   it { should respond_to(:email) }
   it { should respond_to(:password_digest) }
